8ac1a8bc81091932732f44002fd9d5dcf5fd6e7b
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-10-04 07:43:32 +0100

    strings method

--- index.js
+++ index.js
@@ -1,23 +1,95 @@
-const a = 1;
-console.log(a)
+// const a = 1;
+// console.log(a)
 
-const b = "string";
-console.log(b)
+// const b = "string";
+// console.log(b)
 
-const c = true;
-console.log(c)
+// const c = true;
+// console.log(c)
 
-const d = null; 
-console.log(d)
+// const d = null; 
+// console.log(d)
 
-const e = undefined;
-console.log(e)
+// const e = undefined;
+// console.log(e)
 
 
-const name = 'Viktrified';
-let age = "80";
-let isOld = true;
-const lastName = 'Lanje';
-const nationality = 'Nigeria'
+// const name = 'Viktrified';
+// let age = "80";
+// let isOld = true;
+// const lastName = 'Lanje';
+// const nationality = 'Nigeria'
 
-console.log(name);
+// console.log(name);
+
+// let nickname = new String("mano")
+
+// let len = nationality.length;
+// let nick_len = nickname.length;
+
+// let index_char = nationality.charAt(5);
+
+// console.log(nick_char);
+
+// let name = "Victory";
+// console.log(name);
+
+// let age = 21; // declaration 
+// console.log(age);
+
+// age = 22; // assignment
+// console.log(age);
+
+// let year = 2023;
+// console.log(year);
+
+// year = 2024;
+// console.log(year);
+
+let text = "  ourOwnWord  ";
+// console.log(text);
+let text2 = "allNightSweet";
+
+let howMany = text.length;
+// console.log(howMany);
+
+let index = text.charAt(4);
+// console.log(index);
+
+let why = text.charCodeAt(2);
+// console.log(why);
+
+let index2 = text.at(2);
+// console.log(at);
+
+let indexMain = text[6];
+// console.log(indexMain);
+
+const sliced = text.slice(3, 6);
+// console.log(sliced);
+
+const sameAsAbove = text.substring(3, 6);
+// console.log(sameAsAbove);
+
+let sliceStartAndLength = text.substr(3, 10)
+// console.log(sliceStartAndLength);
+
+sliceStartAndLength = text.toLocaleUpperCase();
+// console.log(sliceStartAndLength);
+
+const hustling = text.toLocaleLowerCase();
+// console.log(hustling);
+
+const concatenate = text.concat(" ", text2, " anotherSpace ", "all the space sef");
+// console.log(concatenate);
+
+let newSpace = text.trim();
+// console.log(newSpace)
+
+let notUse = "  spaced  ";
+notUse = notUse.trimStart();
+// console.log(notUse)
+
+let wahala = "  notTired  ";
+// console.log(wahala);
+
